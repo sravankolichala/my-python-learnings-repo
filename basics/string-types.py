094c0e2f852e25a9eddd7e722db1cf885b7ac3e1
Sravan Kolichala <sravankolichala@gmail.com>
2017-11-26 01:10:10 +0530

    String parsing for multiple char-sets and escape characters

---
+++ basics/string-types.py
@@ -0,0 +1,9 @@
+# To print a string normal way
+print('apple')
+
+# To print a string with UNICODE text
+print(u'äþþ􄦛􄥸')
+
+# To print a string with RAW format.. i.e., no further parsing of escape characters in the string
+print(r'c:\temp\dir')
+
